deef0c4af5aef02abf6076ee4f632849daf55374
Marc <g40419@gmail.com>
2021-02-27 17:08:31 +0800

    create project

---
+++ srv/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+// import socketIO from "socket.io";
+
+export default (app, http) => {
+  app.use(express.json())
+  app.use(cors())
+  require('./routes/index')(app)
+  //
+  // app.get('/foo', (req, res) => {
+  //   res.json({msg: 'foo'});
+  // });
+  //
+  // app.post('/bar', (req, res) => {
+  //   res.json(req.body);
+  // });
+  //
+  // optional support for socket.io
+  //
+  // let io = socketIO(http);
+  // io.on("connection", client => {
+  //   client.on("message", function(data) {
+  //     // do something
+  //   });
+  //   client.emit("message", "Welcome");
+  // });
+}
